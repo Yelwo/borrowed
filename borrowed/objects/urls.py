86f344af262fd46b7c3ead6bd7935037d16d7d70
Filip Wolwowicz <filip.wolwowicz@gmail.com>
2023-12-12 15:35:16 +0100

    Add Lent and Borrowed lists

--- borrowed/objects/urls.py
+++ borrowed/objects/urls.py
@@ -11,4 +11,6 @@ router.register(r'borrows', views.BorrowViewSet, basename='borrows')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('borrows/lent/', views.BorrowViewSet.as_view({"list": "lent"})),
+    path('borrows/borrowed/', views.BorrowViewSet.as_view({"list": "borrowed"})),
 ]
--- borrowed/objects/views.py
+++ borrowed/objects/views.py
@@ -1,4 +1,5 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, mixins, response
+from rest_framework.decorators import action
 
 from . import models
 from . import serializers
@@ -12,12 +13,29 @@ class ObjectViewSet(viewsets.ModelViewSet):
         return models.Object.objects.filter(owner__user=self.request.user)
 
 
-class BorrowViewSet(viewsets.ModelViewSet):
+class BorrowViewSet(
+    mixins.CreateModelMixin, 
+    mixins.RetrieveModelMixin, 
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     serializer_class = serializers.BorrowSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=["get"])
+    def lent(self, request):
+        lent = models.Borrow.objects.filter(object__owner__user=self.request.user)        
+        serializer = self.get_serializer(lent, many=True)
+        return response.Response(serializer.data)
+    
+    @action(detail=False, methods=["get"])
+    def borrowed(self, request):
+        borrowed = models.Borrow.objects.filter(borrower__user=self.request.user)
+        serializer = self.get_serializer(borrowed, many=True)
+        return response.Response(serializer.data)
 
     def get_queryset(self):
         lent = models.Borrow.objects.filter(object__owner__user=self.request.user)
         borrowed = models.Borrow.objects.filter(borrower__user=self.request.user)
-        return lent.union(borrowed)
-        
+        return lent | borrowed
